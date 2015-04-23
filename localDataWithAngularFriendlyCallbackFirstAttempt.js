c0a1295b2a8c79d0ccf4eb8e52037b897c448bc1
Dominic McAdden <dominic.mcadden@gmail.com>
2015-04-23 13:04:55 +0100

    turned this into a fascinating 5 part story worthy of Shakespeare

---
+++ localDataWithAngularFriendlyCallbackFirstAttempt.js
@@ -0,0 +1,229 @@
+JSON_CALLBACK({
+		"title": "Recent Uploads tagged monkey",
+		"link": "http://www.flickr.com/photos/tags/monkey/",
+		"description": "",
+		"modified": "2015-04-23T07:58:35Z",
+		"generator": "http://www.flickr.com/",
+		"items": [
+	   {
+			"title": "Waiting for the stars of the show to arrive #dolphins #monkeymia by jillianmchugh http://evth.in/1Dldd7H",
+			"link": "http://www.flickr.com/photos/91531324@N08/17056227429/",
+			"media": {"m":"http://farm8.staticflickr.com/7637/17056227429_6c5638e28b_m.jpg"},
+			"date_taken": "2015-04-23T00:58:35-08:00",
+			"description": " <p><a href=\"http://www.flickr.com/people/91531324@N08/\">everythinggeraldton<\/a> posted a photo:<\/p> <p><a href=\"http://www.flickr.com/photos/91531324@N08/17056227429/\" title=\"Waiting for the stars of the show to arrive #dolphins #monkeymia by jillianmchugh http://evth.in/1Dldd7H\"><img src=\"http://farm8.staticflickr.com/7637/17056227429_6c5638e28b_m.jpg\" width=\"240\" height=\"240\" alt=\"Waiting for the stars of the show to arrive #dolphins #monkeymia by jillianmchugh http://evth.in/1Dldd7H\" /><\/a><\/p> <p><a href=\"http://evth.in/1Hpo0UI\" rel=\"nofollow\">evth.in/1Hpo0UI<\/a><\/p>",
+			"published": "2015-04-23T07:58:35Z",
+			"author": "nobody@flickr.com (everythinggeraldton)",
+			"author_id": "91531324@N08",
+			"tags": "monkey bay shark mia everything monkeymia geraldton everythinggeraldton gerogram"
+	   },
+	   {
+			"title": "Kyoto 042315",
+			"link": "http://www.flickr.com/photos/hideakihatta/17056108199/",
+			"media": {"m":"http://farm8.staticflickr.com/7682/17056108199_eea0a697c4_m.jpg"},
+			"date_taken": "2015-04-23T15:02:42-08:00",
+			"description": " <p><a href=\"http://www.flickr.com/people/hideakihatta/\">HideakiHatta<\/a> posted a photo:<\/p> <p><a href=\"http://www.flickr.com/photos/hideakihatta/17056108199/\" title=\"Kyoto 042315\"><img src=\"http://farm8.staticflickr.com/7682/17056108199_eea0a697c4_m.jpg\" width=\"240\" height=\"160\" alt=\"Kyoto 042315\" /><\/a><\/p> <p><\/p>",
+			"published": "2015-04-23T07:40:28Z",
+			"author": "nobody@flickr.com (HideakiHatta)",
+			"author_id": "68647939@N07",
+			"tags": "monkey bambooshoot"
+	   },
+	   {
+			"title": "Kyoto 042315",
+			"link": "http://www.flickr.com/photos/hideakihatta/17241740281/",
+			"media": {"m":"http://farm8.staticflickr.com/7622/17241740281_b9dfb99337_m.jpg"},
+			"date_taken": "2015-04-23T15:05:23-08:00",
+			"description": " <p><a href=\"http://www.flickr.com/people/hideakihatta/\">HideakiHatta<\/a> posted a photo:<\/p> <p><a href=\"http://www.flickr.com/photos/hideakihatta/17241740281/\" title=\"Kyoto 042315\"><img src=\"http://farm8.staticflickr.com/7622/17241740281_b9dfb99337_m.jpg\" width=\"160\" height=\"240\" alt=\"Kyoto 042315\" /><\/a><\/p> <p><\/p>",
+			"published": "2015-04-23T07:40:30Z",
+			"author": "nobody@flickr.com (HideakiHatta)",
+			"author_id": "68647939@N07",
+			"tags": "monkey bambooshoot"
+	   },
+	   {
+			"title": "Kyoto 042315",
+			"link": "http://www.flickr.com/photos/hideakihatta/17241740601/",
+			"media": {"m":"http://farm8.staticflickr.com/7665/17241740601_95e434d2d7_m.jpg"},
+			"date_taken": "2015-04-23T15:03:24-08:00",
+			"description": " <p><a href=\"http://www.flickr.com/people/hideakihatta/\">HideakiHatta<\/a> posted a photo:<\/p> <p><a href=\"http://www.flickr.com/photos/hideakihatta/17241740601/\" title=\"Kyoto 042315\"><img src=\"http://farm8.staticflickr.com/7665/17241740601_95e434d2d7_m.jpg\" width=\"240\" height=\"160\" alt=\"Kyoto 042315\" /><\/a><\/p> <p><\/p>",
+			"published": "2015-04-23T07:40:29Z",
+			"author": "nobody@flickr.com (HideakiHatta)",
+			"author_id": "68647939@N07",
+			"tags": "monkey bambooshoot"
+	   },
+	   {
+			"title": "Kyoto 042315",
+			"link": "http://www.flickr.com/photos/hideakihatta/17240561172/",
+			"media": {"m":"http://farm8.staticflickr.com/7604/17240561172_be9b18721b_m.jpg"},
+			"date_taken": "2015-04-23T15:02:27-08:00",
+			"description": " <p><a href=\"http://www.flickr.com/people/hideakihatta/\">HideakiHatta<\/a> posted a photo:<\/p> <p><a href=\"http://www.flickr.com/photos/hideakihatta/17240561172/\" title=\"Kyoto 042315\"><img src=\"http://farm8.staticflickr.com/7604/17240561172_be9b18721b_m.jpg\" width=\"240\" height=\"160\" alt=\"Kyoto 042315\" /><\/a><\/p> <p><\/p>",
+			"published": "2015-04-23T07:40:27Z",
+			"author": "nobody@flickr.com (HideakiHatta)",
+			"author_id": "68647939@N07",
+			"tags": "monkey bambooshoot"
+	   },
+	   {
+			"title": "Kyoto 042315",
+			"link": "http://www.flickr.com/photos/hideakihatta/17241741651/",
+			"media": {"m":"http://farm8.staticflickr.com/7630/17241741651_c3d01948ef_m.jpg"},
+			"date_taken": "2015-04-23T15:02:29-08:00",
+			"description": " <p><a href=\"http://www.flickr.com/people/hideakihatta/\">HideakiHatta<\/a> posted a photo:<\/p> <p><a href=\"http://www.flickr.com/photos/hideakihatta/17241741651/\" title=\"Kyoto 042315\"><img src=\"http://farm8.staticflickr.com/7630/17241741651_c3d01948ef_m.jpg\" width=\"240\" height=\"160\" alt=\"Kyoto 042315\" /><\/a><\/p> <p><\/p>",
+			"published": "2015-04-23T07:40:27Z",
+			"author": "nobody@flickr.com (HideakiHatta)",
+			"author_id": "68647939@N07",
+			"tags": "monkey bambooshoot"
+	   },
+	   {
+			"title": "Kyoto 042315",
+			"link": "http://www.flickr.com/photos/hideakihatta/17054748970/",
+			"media": {"m":"http://farm8.staticflickr.com/7658/17054748970_06d83b32d6_m.jpg"},
+			"date_taken": "2015-04-23T15:02:33-08:00",
+			"description": " <p><a href=\"http://www.flickr.com/people/hideakihatta/\">HideakiHatta<\/a> posted a photo:<\/p> <p><a href=\"http://www.flickr.com/photos/hideakihatta/17054748970/\" title=\"Kyoto 042315\"><img src=\"http://farm8.staticflickr.com/7658/17054748970_06d83b32d6_m.jpg\" width=\"240\" height=\"160\" alt=\"Kyoto 042315\" /><\/a><\/p> <p><\/p>",
+			"published": "2015-04-23T07:40:28Z",
+			"author": "nobody@flickr.com (HideakiHatta)",
+			"author_id": "68647939@N07",
+			"tags": "monkey bambooshoot"
+	   },
+	   {
+			"title": "Kyoto 042315",
+			"link": "http://www.flickr.com/photos/hideakihatta/17056106849/",
+			"media": {"m":"http://farm6.staticflickr.com/5323/17056106849_ae21b651bd_m.jpg"},
+			"date_taken": "2015-04-23T15:05:28-08:00",
+			"description": " <p><a href=\"http://www.flickr.com/people/hideakihatta/\">HideakiHatta<\/a> posted a photo:<\/p> <p><a href=\"http://www.flickr.com/photos/hideakihatta/17056106849/\" title=\"Kyoto 042315\"><img src=\"http://farm6.staticflickr.com/5323/17056106849_ae21b651bd_m.jpg\" width=\"160\" height=\"240\" alt=\"Kyoto 042315\" /><\/a><\/p> <p><\/p>",
+			"published": "2015-04-23T07:40:31Z",
+			"author": "nobody@flickr.com (HideakiHatta)",
+			"author_id": "68647939@N07",
+			"tags": "monkey bambooshoot"
+	   },
+	   {
+			"title": "Kyoto 042315",
+			"link": "http://www.flickr.com/photos/hideakihatta/17242294525/",
+			"media": {"m":"http://farm8.staticflickr.com/7641/17242294525_6293fe7d37_m.jpg"},
+			"date_taken": "2015-04-23T15:02:20-08:00",
+			"description": " <p><a href=\"http://www.flickr.com/people/hideakihatta/\">HideakiHatta<\/a> posted a photo:<\/p> <p><a href=\"http://www.flickr.com/photos/hideakihatta/17242294525/\" title=\"Kyoto 042315\"><img src=\"http://farm8.staticflickr.com/7641/17242294525_6293fe7d37_m.jpg\" width=\"160\" height=\"240\" alt=\"Kyoto 042315\" /><\/a><\/p> <p><\/p>",
+			"published": "2015-04-23T07:40:26Z",
+			"author": "nobody@flickr.com (HideakiHatta)",
+			"author_id": "68647939@N07",
+			"tags": "monkey bambooshoot"
+	   },
+	   {
+			"title": "Kyoto 042315",
+			"link": "http://www.flickr.com/photos/hideakihatta/17056111579/",
+			"media": {"m":"http://farm8.staticflickr.com/7619/17056111579_be79be2986_m.jpg"},
+			"date_taken": "2015-04-23T15:01:06-08:00",
+			"description": " <p><a href=\"http://www.flickr.com/people/hideakihatta/\">HideakiHatta<\/a> posted a photo:<\/p> <p><a href=\"http://www.flickr.com/photos/hideakihatta/17056111579/\" title=\"Kyoto 042315\"><img src=\"http://farm8.staticflickr.com/7619/17056111579_be79be2986_m.jpg\" width=\"240\" height=\"160\" alt=\"Kyoto 042315\" /><\/a><\/p> <p><\/p>",
+			"published": "2015-04-23T07:40:22Z",
+			"author": "nobody@flickr.com (HideakiHatta)",
+			"author_id": "68647939@N07",
+			"tags": "monkey bambooshoot"
+	   },
+	   {
+			"title": "Kyoto 042315",
+			"link": "http://www.flickr.com/photos/hideakihatta/16619845074/",
+			"media": {"m":"http://farm9.staticflickr.com/8700/16619845074_a7be011a86_m.jpg"},
+			"date_taken": "2015-04-23T15:02:55-08:00",
+			"description": " <p><a href=\"http://www.flickr.com/people/hideakihatta/\">HideakiHatta<\/a> posted a photo:<\/p> <p><a href=\"http://www.flickr.com/photos/hideakihatta/16619845074/\" title=\"Kyoto 042315\"><img src=\"http://farm9.staticflickr.com/8700/16619845074_a7be011a86_m.jpg\" width=\"240\" height=\"160\" alt=\"Kyoto 042315\" /><\/a><\/p> <p><\/p>",
+			"published": "2015-04-23T07:40:29Z",
+			"author": "nobody@flickr.com (HideakiHatta)",
+			"author_id": "68647939@N07",
+			"tags": "monkey bambooshoot"
+	   },
+	   {
+			"title": "Kyoto 042315",
+			"link": "http://www.flickr.com/photos/hideakihatta/17054751660/",
+			"media": {"m":"http://farm8.staticflickr.com/7624/17054751660_e8c23302cf_m.jpg"},
+			"date_taken": "2015-04-23T15:01:08-08:00",
+			"description": " <p><a href=\"http://www.flickr.com/people/hideakihatta/\">HideakiHatta<\/a> posted a photo:<\/p> <p><a href=\"http://www.flickr.com/photos/hideakihatta/17054751660/\" title=\"Kyoto 042315\"><img src=\"http://farm8.staticflickr.com/7624/17054751660_e8c23302cf_m.jpg\" width=\"240\" height=\"160\" alt=\"Kyoto 042315\" /><\/a><\/p> <p><\/p>",
+			"published": "2015-04-23T07:40:23Z",
+			"author": "nobody@flickr.com (HideakiHatta)",
+			"author_id": "68647939@N07",
+			"tags": "monkey bambooshoot"
+	   },
+	   {
+			"title": "Kyoto 042315",
+			"link": "http://www.flickr.com/photos/hideakihatta/17242292535/",
+			"media": {"m":"http://farm9.staticflickr.com/8776/17242292535_3177e2a6ba_m.jpg"},
+			"date_taken": "2015-04-23T15:05:47-08:00",
+			"description": " <p><a href=\"http://www.flickr.com/people/hideakihatta/\">HideakiHatta<\/a> posted a photo:<\/p> <p><a href=\"http://www.flickr.com/photos/hideakihatta/17242292535/\" title=\"Kyoto 042315\"><img src=\"http://farm9.staticflickr.com/8776/17242292535_3177e2a6ba_m.jpg\" width=\"160\" height=\"240\" alt=\"Kyoto 042315\" /><\/a><\/p> <p><\/p>",
+			"published": "2015-04-23T07:40:31Z",
+			"author": "nobody@flickr.com (HideakiHatta)",
+			"author_id": "68647939@N07",
+			"tags": "monkey bambooshoot"
+	   },
+	   {
+			"title": "Kyoto 042315",
+			"link": "http://www.flickr.com/photos/hideakihatta/16619847554/",
+			"media": {"m":"http://farm9.staticflickr.com/8760/16619847554_2f2b22794f_m.jpg"},
+			"date_taken": "2015-04-23T15:01:11-08:00",
+			"description": " <p><a href=\"http://www.flickr.com/people/hideakihatta/\">HideakiHatta<\/a> posted a photo:<\/p> <p><a href=\"http://www.flickr.com/photos/hideakihatta/16619847554/\" title=\"Kyoto 042315\"><img src=\"http://farm9.staticflickr.com/8760/16619847554_2f2b22794f_m.jpg\" width=\"240\" height=\"160\" alt=\"Kyoto 042315\" /><\/a><\/p> <p><\/p>",
+			"published": "2015-04-23T07:40:24Z",
+			"author": "nobody@flickr.com (HideakiHatta)",
+			"author_id": "68647939@N07",
+			"tags": "monkey bambooshoot"
+	   },
+	   {
+			"title": "Kyoto 042315",
+			"link": "http://www.flickr.com/photos/hideakihatta/17240563312/",
+			"media": {"m":"http://farm6.staticflickr.com/5449/17240563312_d519abb435_m.jpg"},
+			"date_taken": "2015-04-23T15:01:10-08:00",
+			"description": " <p><a href=\"http://www.flickr.com/people/hideakihatta/\">HideakiHatta<\/a> posted a photo:<\/p> <p><a href=\"http://www.flickr.com/photos/hideakihatta/17240563312/\" title=\"Kyoto 042315\"><img src=\"http://farm6.staticflickr.com/5449/17240563312_d519abb435_m.jpg\" width=\"240\" height=\"160\" alt=\"Kyoto 042315\" /><\/a><\/p> <p><\/p>",
+			"published": "2015-04-23T07:40:23Z",
+			"author": "nobody@flickr.com (HideakiHatta)",
+			"author_id": "68647939@N07",
+			"tags": "monkey bambooshoot"
+	   },
+	   {
+			"title": "Kyoto 042315",
+			"link": "http://www.flickr.com/photos/hideakihatta/17242293635/",
+			"media": {"m":"http://farm9.staticflickr.com/8765/17242293635_a8c649b913_m.jpg"},
+			"date_taken": "2015-04-23T15:02:46-08:00",
+			"description": " <p><a href=\"http://www.flickr.com/people/hideakihatta/\">HideakiHatta<\/a> posted a photo:<\/p> <p><a href=\"http://www.flickr.com/photos/hideakihatta/17242293635/\" title=\"Kyoto 042315\"><img src=\"http://farm9.staticflickr.com/8765/17242293635_a8c649b913_m.jpg\" width=\"240\" height=\"160\" alt=\"Kyoto 042315\" /><\/a><\/p> <p><\/p>",
+			"published": "2015-04-23T07:40:28Z",
+			"author": "nobody@flickr.com (HideakiHatta)",
+			"author_id": "68647939@N07",
+			"tags": "monkey bambooshoot"
+	   },
+	   {
+			"title": "Kyoto 042315",
+			"link": "http://www.flickr.com/photos/hideakihatta/17056111739/",
+			"media": {"m":"http://farm8.staticflickr.com/7646/17056111739_b0c8832c7f_m.jpg"},
+			"date_taken": "2015-04-23T15:01:04-08:00",
+			"description": " <p><a href=\"http://www.flickr.com/people/hideakihatta/\">HideakiHatta<\/a> posted a photo:<\/p> <p><a href=\"http://www.flickr.com/photos/hideakihatta/17056111739/\" title=\"Kyoto 042315\"><img src=\"http://farm8.staticflickr.com/7646/17056111739_b0c8832c7f_m.jpg\" width=\"240\" height=\"160\" alt=\"Kyoto 042315\" /><\/a><\/p> <p><\/p>",
+			"published": "2015-04-23T07:40:21Z",
+			"author": "nobody@flickr.com (HideakiHatta)",
+			"author_id": "68647939@N07",
+			"tags": "monkey bambooshoot"
+	   },
+	   {
+			"title": "Kyoto 042315",
+			"link": "http://www.flickr.com/photos/hideakihatta/16619846614/",
+			"media": {"m":"http://farm8.staticflickr.com/7657/16619846614_f7e7f7a802_m.jpg"},
+			"date_taken": "2015-04-23T15:01:37-08:00",
+			"description": " <p><a href=\"http://www.flickr.com/people/hideakihatta/\">HideakiHatta<\/a> posted a photo:<\/p> <p><a href=\"http://www.flickr.com/photos/hideakihatta/16619846614/\" title=\"Kyoto 042315\"><img src=\"http://farm8.staticflickr.com/7657/16619846614_f7e7f7a802_m.jpg\" width=\"240\" height=\"160\" alt=\"Kyoto 042315\" /><\/a><\/p> <p><\/p>",
+			"published": "2015-04-23T07:40:26Z",
+			"author": "nobody@flickr.com (HideakiHatta)",
+			"author_id": "68647939@N07",
+			"tags": "monkey bambooshoot"
+	   },
+	   {
+			"title": "Kyoto 042315",
+			"link": "http://www.flickr.com/photos/hideakihatta/16619846764/",
+			"media": {"m":"http://farm9.staticflickr.com/8686/16619846764_e121da05d8_m.jpg"},
+			"date_taken": "2015-04-23T15:01:35-08:00",
+			"description": " <p><a href=\"http://www.flickr.com/people/hideakihatta/\">HideakiHatta<\/a> posted a photo:<\/p> <p><a href=\"http://www.flickr.com/photos/hideakihatta/16619846764/\" title=\"Kyoto 042315\"><img src=\"http://farm9.staticflickr.com/8686/16619846764_e121da05d8_m.jpg\" width=\"240\" height=\"160\" alt=\"Kyoto 042315\" /><\/a><\/p> <p><\/p>",
+			"published": "2015-04-23T07:40:25Z",
+			"author": "nobody@flickr.com (HideakiHatta)",
+			"author_id": "68647939@N07",
+			"tags": "monkey bambooshoot"
+	   },
+	   {
+			"title": "Kyoto 042315",
+			"link": "http://www.flickr.com/photos/hideakihatta/17242295325/",
+			"media": {"m":"http://farm8.staticflickr.com/7677/17242295325_6cecc18836_m.jpg"},
+			"date_taken": "2015-04-23T15:01:14-08:00",
+			"description": " <p><a href=\"http://www.flickr.com/people/hideakihatta/\">HideakiHatta<\/a> posted a photo:<\/p> <p><a href=\"http://www.flickr.com/photos/hideakihatta/17242295325/\" title=\"Kyoto 042315\"><img src=\"http://farm8.staticflickr.com/7677/17242295325_6cecc18836_m.jpg\" width=\"240\" height=\"160\" alt=\"Kyoto 042315\" /><\/a><\/p> <p><\/p>",
+			"published": "2015-04-23T07:40:24Z",
+			"author": "nobody@flickr.com (HideakiHatta)",
+			"author_id": "68647939@N07",
+			"tags": "monkey bambooshoot"
+	   }
+        ]
+})
